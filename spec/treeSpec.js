12aef71196157e5856f3ed6fc7aed935d2b36bca
Hack Reactor Students <github@hackreactor.com>
2013-06-20 12:42:34 -0700

    some tests pass specs for .addChild

--- spec/treeSpec.js
+++ spec/treeSpec.js
@@ -11,5 +11,26 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
+  describe(".addChild", function(){
+    it("should be able to add a child to an empty tree", function() {
+      tree.addChild("test");
+      expect(tree.value).toEqual("test");
+    });
+
+    it("should be able to add a child to a node", function() {
+      tree.addChild("test");
+      tree.addChild("child");
+      expect(tree.children.value).toEqual("child");
+    });
+
+    it("should be able to add two children to the parent node", function() {
+      tree.addChild("parent");
+      tree.addChild("child1");
+      tree.addChild("child2");
+      expect(tree.children).toContain("child2");
+      expect(tree.children).toContain("child1");
+    });
+  });
+
   // Add more tests here to test the functionality of tree.
 });
--- src/tree.js
+++ src/tree.js
@@ -3,13 +3,23 @@ var makeTree = function(){
   newTree.value = undefined;
   newTree.children = undefined;
 
+  _.extend(newTree, makeTree.treeMethods);
   return newTree;
 };
 
-var treeMethods = {};
+makeTree.treeMethods = {};
 
-treeMethods.addChild = function(){
+makeTree.treeMethods.addChild = function(val){
+  if (this.value){
+    if (!this.children){
+      this.children = [];
+    }
+    this.children.value = val;
+    this.children.push(val);
+  } else {
+    this.value = val;
+  }
 };
 
-treeMethods.contains = function(){
+makeTree.treeMethods.contains = function(){
 };
