d39a513dea42c29519bbbb1cf7594c1e908e089b
Hack Reactor Students <github@hackreactor.com>
2013-06-20 17:12:06 -0700

    Contains works! could use refactor, logic is messy

--- spec/treeSpec.js
+++ spec/treeSpec.js
@@ -20,26 +20,50 @@ describe("tree", function() {
     it("should be able to add a child to a node", function() {
       tree.addChild("test");
       tree.addChild("child");
-      expect(tree.children.value).toEqual("child");
+      expect(tree.children[0].value).toEqual("child");
     });
 
     it("should be able to add two children to the parent node", function() {
       tree.addChild("parent");
       tree.addChild("child1");
       tree.addChild("child2");
-      expect(tree.children).toContain("child2");
-      expect(tree.children).toContain("child1");
+      expect(tree.children[1].value).toContain("child2");
+      expect(tree.children[0].value).toContain("child1");
     });
+
+    it("should be able to add children on any level", function() {
+      tree.addChild("parent");
+      tree.addChild("child1");
+      tree.addChild("child2");
+      tree.children[1].addChild("child of child2");
+      expect(tree.children[1].children[0].value).toContain("child of child2");
+    });
+
   });
   describe(".contains", function(){
     it("should return false when there is no node", function() {
       expect(tree.contains("bubblegum")).toEqual(false);
     });
+
     it("should return true when the node does exist", function() {
       tree.addChild("test");
       expect(tree.contains("test")).toEqual(true);
     });
 
+    it("should check all nodes for the value", function() {
+      tree.addChild("test");
+      tree.addChild("node");
+      tree.addChild("another");
+      tree.addChild("yeah!");
+      expect(tree.contains("another")).toEqual(true);
+    });
+
+    it("should multiple levels of the tree for the value", function() {
+      tree.addChild("test");
+      tree.addChild("node");
+      tree.children[0].addChild("nodeChild");
+      expect(tree.contains("nodeChild")).toEqual(true);
+    });
   });
   // Add more tests here to test the functionality of tree.
 });
--- src/tree.js
+++ src/tree.js
@@ -1,34 +1,80 @@
 var makeTree = function(){
-  var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
-
+  var newTree = {
+    value: undefined,
+    children: []
+  };
   _.extend(newTree, makeTree.treeMethods);
   return newTree;
 };
 
 makeTree.treeMethods = {};
 
+// target is optional if specific node is desired
 makeTree.treeMethods.addChild = function(val){
+  var node = makeTree();
+  node.value = val;
+  node.children = [];
+
   if (this.value){
-    if (!this.children){
-      this.children = [];
-    }
-    this.children.value = val;
-    this.children.push(val);
+    this.children.push(node);
   } else {
-    this.value = val;
+    this.value= val;
   }
 };
 
 makeTree.treeMethods.contains = function(val){
+  var level = this.children;
+  var tempLevel = level;
+  var numLevels = 1;
   if (this.value){
-    if (this.value === true) {
+    if (this.value === val) {
       return true;
     } else {
-      this.children.contains(val);
+      for (var tier = 0; tier < numLevels; tier++) {
+        for (var nodes = 0; nodes < level.length; nodes++) {
+          if (level[nodes].value === val) { return true; }
+          if (level[nodes].children) {
+            tempLevel = level[nodes].children;
+          }
+        }
+        if (tempLevel !== level) {
+          level = tempLevel;
+          numLevels++;
+        }
+      }
     }
-  } else {
-    return false;
-  } 
+  }
+  return false;
 };
+
+// if tree is empty, then value of the tree sends in the passed in value; 
+// otherwise the object is passed in to the array with value equal to the passed in value and children has blank array
+
+// var tree = {
+//   value: "test",
+//   children: [{{
+//     value: "node",
+//     children: [{
+//       value: "another",
+//       children: [{
+//         value: "okay"
+//         children: undefined
+//       }]
+//     }]
+//   },{
+//     value: "next node"
+//     children: [{
+//       value: "and here we go"
+//       children: undefined
+//     }]
+//   }}]
+// }
+
+// {
+//   tree:{
+//     test:undefined
+//     node:undefined
+//     another:
+//   }
+//  }
+// }
